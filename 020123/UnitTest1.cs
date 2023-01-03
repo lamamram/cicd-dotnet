d537b404e5e66a07a2606dc74f0993928ec69985
matt <mlamamra@dawan.fr>
2023-01-03 16:37:06 +0100

    récupération fichier csproj && unit test

---
+++ 020123/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _020123
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
