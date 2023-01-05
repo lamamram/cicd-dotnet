b60d81a28f82653c4a3a08def6d54ffdd75df486
Administrator <admin@example.com>
2023-01-05 13:13:52 +0000

    Squash Resolve "ajouter deux entiers"
    - test unitaire
    - fonctionnalité
    - job unit test

---
+++ Calculator/Adder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Adder
+    {
+        public static int Add(int x, int y) {
+            return x + y;
+        }
+    }
+}
