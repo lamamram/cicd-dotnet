560affecd7b01ffae5b1479b22676b17777ca966
matt <mlamamra@dawan.fr>
2023-01-05 14:52:23 +0100

    force dependency download

--- Calculator/Adder.cs
+++ Calculator/Adder.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using OpenQA.Selenium.Firefox;
 
 namespace Calculator
 {
     public class Adder
     {
+        FirefoxOptions options = new FirefoxOptions();
         public static int Add(int x, int y) {
             return x + y;
         }
