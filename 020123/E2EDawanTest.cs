d99b912063abf105e1c621c80988ed85e31d34d9
matt <mlamamra@dawan.fr>
2023-01-06 17:09:39 +0100

    test e2e

---
+++ 020123/E2EDawanTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+
+
+namespace _020123
+{
+    [TestClass]
+    public class E2EDawanTest
+    {
+
+        [TestMethod, TestCategory("E2E")]
+        public void TestHomeTitle()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArgument("--headless");
+            //IWebDriver driver = new FirefoxDriver(options);
+            RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://selenium_server:4444/wd/hub"), options: options);
+            driver.Url = "https://dawan.fr";
+            StringAssert.Contains(driver.Title, "Dawan");
+            driver.Close();
+        }
+    }
+}
